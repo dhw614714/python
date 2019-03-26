7df904f2957326152c58cfbf67729cfd1dab49db
dhw614714 <2223018083@qq.com>
2019-03-26 15:55:50 +0800

    0x01abs.py

---
+++ 0x01abs.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# @Time   : 2019/3/26 15:30
+# @Author : liaochao
+# @File   : 0x01abs.py
+print ("abs(-40) : ", abs(-40))
+print ("abs(100.10) : ", abs(100.10))
